0524be2932f3140621a24d108979eac9314260ad
Siguhau <siguhau@stud.ntnu.no>
2022-10-06 15:30:47 +0200

    Add: gpu support

--- src/ffmpeg_general_compression.py
+++ src/ffmpeg_general_compression.py
@@ -3,7 +3,9 @@ import subprocess
 import time
 
 
-def general_compression(input_folder, output_path, codec, cr):
+def general_compression(
+    input_folder, output_path, codec="libx265", compression_rate=20, use_gpu=False
+):
     general_output_path = os.path.join(output_path, "compressed")
     try:
         os.mkdir(general_output_path)
@@ -20,8 +22,9 @@ def general_compression(input_folder, output_path, codec, cr):
         start_time = time.time()
         output_path = os.path.join(general_output_path, f"{idx}.mp4")
         input_path = os.path.join(input_folder, video)
-        print(output_path)
-        command = f"ffmpeg -hwaccel cuda -i {input_path} -vcodec {codec} -crf {cr} {output_path}"
-        print(command)
+        command = f"ffmpeg -i {input_path} -vcodec {codec} -crf {compression_rate} {output_path}"
+        if use_gpu:
+            output_path = os.path.join(general_output_path, f"{idx}.avi")
+            command = f"ffmpeg -i {input_path} -c:v h264_nvenc -preset:v p7 -tune hq -cq:v 19 -rc:v vbr {output_path}"
         subprocess.call(command.split())
         print("time: ", time.time() - start_time)
