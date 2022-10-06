4cadf64ea6ae230b50baa1cf2f1e06632d92ceed
Ellen Yu <ellenyu1999@hotmail.com>
2022-10-06 12:55:37 +0200

    Changed to functions in files

--- src/ffmpeg_general_compression.py
+++ src/ffmpeg_general_compression.py
@@ -10,34 +10,34 @@
 # might need to install ffmpeg to your computer
 
 
-import argparse
+#import argparse
 import os
 import subprocess
 import time
 
-if __name__ == "__main__":
-
-    # Argparser
-    ap = argparse.ArgumentParser(description="Get compression information")
-    ap.add_argument("-i", "--input", required=True, help="input folder path")
-    ap.add_argument(
-        "-o", "--output", required=False, default=os.getcwd(), help="output file"
-    )
-    ap.add_argument("-c", "--codec", required=True, help="Codec")
-    ap.add_argument(
-        "-cr", "--compressionRate", required=True, help="Level of compression"
-    )
-    args = vars(ap.parse_args())
-
+def general_compression(input, output, codec, cr):
+    
+     # Argparser
+    # ap = argparse.ArgumentParser(description="Get compression information")
+    # ap.add_argument("-i", "--input", required=True, help="input folder path")
+    # ap.add_argument(
+    #     "-o", "--output", required=False, default=os.getcwd(), help="output file"
+    # )
+    # ap.add_argument("-c", "--codec", required=True, help="Codec")
+    # ap.add_argument(
+    #     "-cr", "--compressionRate", required=True, help="Level of compression"
+    # )
+    # args = vars(ap.parse_args())
+    
     # Setting arguments
-    output = args["output"]
-    input_arg = args["input"]
-    codec = args["codec"]
-    compression_rate = args["compressionRate"]
-    print(output)
+    # output = args["output"]
+    # input_arg = args["input"]
+    # codec = args["codec"]
+    # compression_rate = args["compressionRate"]
+    #print(output)
     general_output_path = os.path.join(output, "compressed")
     try:
-        print(general_output_path)
+        #print(general_output_path)
         os.mkdir(general_output_path)
     except OSError as e:
         pass
@@ -46,14 +46,17 @@ if __name__ == "__main__":
 
     # Find all files in input folder
     input_videos = [
-        f for f in os.listdir(input_arg) if os.path.isfile(os.path.join(input_arg, f))
+        f for f in os.listdir(input) if os.path.isfile(os.path.join(input, f))
     ]
     for idx, video in enumerate(input_videos):
         start_time = time.time()
         output_path = os.path.join(general_output_path, f"{idx}.mp4")
-        input_path = os.path.join(input_arg, video)
+        input_path = os.path.join(input, video)
         print(output_path)
-        command = f"ffmpeg -hwaccel cuda -i {input_path} -vcodec {codec} -crf {compression_rate} {output_path}"
+        command = f"ffmpeg -hwaccel cuda -i {input_path} -vcodec {codec} -crf {cr} {output_path}"
         print(command)
         subprocess.call(command.split())
         print("time: ", time.time() - start_time)
+        
+if __name__ == "__main__":
+    general_compression("/Users/ellen/Documents/MTDT/TDT4290/FFmpeg-video-compression/src/", "/Users/ellen/Documents/MTDT/TDT4290/FFmpeg-video-compression/src/", "libx", 3)
--- src/framerate.py
+++ src/framerate.py
@@ -1,17 +1,21 @@
 import cv2
 
-vidcap = cv2.VideoCapture("C:\\Users\\sigur\\Desktop\\highframeratevideos\\hpfs2.avi")
-vidcap2 = cv2.VideoCapture("C:\\Users\\sigur\\Desktop\\split\\compressed\\0.mp4")
+def framerate(original_path, compressed_path):
+    
+    "original path: C:\\Users\\sigur\\Desktop\\highframeratevideos\\hpfs2.avi"
+    "compressed path: C:\\Users\\sigur\\Desktop\\split\\compressed\\0.mp4"
+    
+    vidcap = cv2.VideoCapture(original_path)
+    vidcap2 = cv2.VideoCapture(compressed_path)
 
-vidcap.read()
-vidcap2.read()
+    vidcap.read()
+    vidcap2.read()
 
-fps = int(
-    vidcap.get(cv2.CAP_PROP_FPS)
-)  # Might want to go for half of the original fps.
-fps2 = int(
-    vidcap2.get(cv2.CAP_PROP_FPS)
-)  # Might want to go for half of the original fps.
+    fps = int(
+        vidcap.get(cv2.CAP_PROP_FPS)
+    )  # Might want to go for half of the original fps.
+    fps2 = int(
+        vidcap2.get(cv2.CAP_PROP_FPS)
+    )  # Might want to go for half of the original fps.
 
-print(fps)
-print(fps2)
+    return print(fps, fps2)
